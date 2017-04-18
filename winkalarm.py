426a5c42a66f6d69228e7dd28af00109223db9c6
Alex <alancast@umich.edu>
2017-04-17 23:04:00 -0700

    able to dim lights via API!

--- winkalarm.py
+++ winkalarm.py
@@ -1,14 +1,12 @@
 import httplib
-import urllib
 import pywink
-import cgi
 import json
 
 def main():
     infile = open('inputs.txt', 'r')
 
-    # Wasting first 6 lines because that's how I formatted it
-    for x in range(0,6):
+    # Wasting first 7 lines because that's how I formatted it
+    for x in range(0,7):
         infile.readline()
 
     # Read in client secrets and the like
@@ -16,33 +14,61 @@ def main():
     clientSecret = infile.readline().rstrip('\n')
     authUrl = infile.readline().rstrip('\n')
     accessTokenUrl = infile.readline().rstrip('\n')
-    # print clientId, clientSecret, authUrl, accessTokenUrl
+    refreshToken = infile.readline().rstrip('\n')
+    # print clientId, clientSecret, authUrl, accessTokenUrl, refreshToken
 
+    accessToken = AcquireAccessToken(clientId, clientSecret, refreshToken)
+    # print accessToken
+
+    pywink.set_bearer_token(accessToken)
+
+    DimLights()
+    ToggleSwitches()
+    
+    return 0;
+
+def AcquireAccessToken(clientId, clientSecret, refreshToken):
+    # Populate information needed for request
     url = "https://api.wink.com/oauth2/token"
     headers = {"Content-type": "application/json"}
-    password = ""
-    username = "alanc3939+wink@gmail.com"
     body = dict(
         client_id=clientId,
         client_secret=clientSecret,
-        password=password,
-        username=username,
-        grant_type="password"
+        refresh_token=refreshToken,
+        grant_type="refresh_token"
     )
 
+    # Send the request and get the response
     conn = httplib.HTTPSConnection("api.wink.com")
-    url = "https://api.wink.com/oauth2/token"
-    print json.dumps(body)
     conn.request("POST", url = "/oauth2/token" , headers = headers, body = json.dumps(body))
     response = conn.getresponse()
-    print response.status, response.reason
-
-    # pywink.set_bearer_token('YOUR_BEARER_TOKEN')
-    # for switch in pywink.get_switches():
-    #     print(switch.name(), switch.state())
-    #     switch.set_state(not switch.state())
+    if response.status != 200:
+        print response.status, response.reason
+        exit(1)
     
-    return 0;
+    responseStr = response.read().decode('utf-8')
+    responseJson = json.loads(responseStr)
+    # print responseJson
+
+    return responseJson['access_token']
+
+def ToggleSwitches():
+    for switch in pywink.get_switches():
+        print "-------------------------------"
+        name = switch.name()
+        state = switch.state()
+        print name, state
+        print "-------------------------------"
+        switch.set_state(not state)
+
+def DimLights():
+    for bulb in pywink.get_light_bulbs():
+        print "-------------------------------"
+        print(bulb.name(), bulb.state())
+        currentBrightness = bulb.brightness()
+        print currentBrightness
+        bulb.set_state(True, brightness = .6)
+        print "-------------------------------"
 
 if __name__ == "__main__":
     main()
